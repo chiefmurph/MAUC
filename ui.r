ba0ec7e44179ec16e744bb9e186441bb0e6afff1
Dan Murphy <danielmarkmurphy@gmail.com>
2016-02-25 12:01:56 -0800

    removed blank lines

--- ui.r
+++ ui.r
@@ -1,13 +1,9 @@
 shinyUI(fluidPage(
-  
   titlePanel("shiny mauc"),
-  
   sidebarLayout(
     # Inputs excluded for brevity
     ),
-    
-    mainPanel(
-      plotOutput("copula_data_plot")
-    )
+  mainPanel(
+    plotOutput("copula_data_plot")
   )
-))
+  ))
